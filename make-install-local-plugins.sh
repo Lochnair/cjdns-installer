eff08fc8b2865737d1eaa527833c556b511fe116
Nils Andreas Svee <Lochnair@users.noreply.github.com>
2016-12-28 01:30:46 +0100

    Update make-install-local-plugins.sh

--- make-install-local-plugins.sh
+++ make-install-local-plugins.sh
@@ -1,5 +1,5 @@
 my_plugins_path='./dependencies/'
 str1='!addplugindir '"$my_plugins_path"'/NSIS_plugins/Simple_Service_Plugin/'
 str2='!addplugindir '"$my_plugins_path"'/NSIS_plugins/Shelllink/Plugins'
-str3="!define PRODUCT_VERSION $CJDNS_VERSION"
+str3="!define PRODUCT_VERSION $CJDNS_VER"
 makensis -X"$str1" -X"$str2" -X"$str3" installer.nsi
