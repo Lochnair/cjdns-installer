99809e7fabbe846ca57ee0c2f87cee92623f660c
Nils Andreas Svee <Lochnair@users.noreply.github.com>
2016-12-28 01:28:39 +0100

    Pass version from make script

--- make-install-local-plugins.sh
+++ make-install-local-plugins.sh
@@ -1,4 +1,5 @@
 my_plugins_path='./dependencies/'
 str1='!addplugindir '"$my_plugins_path"'/NSIS_plugins/Simple_Service_Plugin/'
 str2='!addplugindir '"$my_plugins_path"'/NSIS_plugins/Shelllink/Plugins'
-makensis -X"$str1" -X"$str2" installer.nsi
+str3="!define PRODUCT_VERSION $CJDNS_VERSION"
+makensis -X"$str1" -X"$str2" -X"$str3" installer.nsi
